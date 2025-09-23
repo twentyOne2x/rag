bb00ce03029f066a0bbe8b19fd7092e5bc283e31
twentyOne2x <contact@attn.market>
2025-09-23 14:23:49 +0200

    feat: ingest yt video

---
+++ src/Llama_index_sandbox/run_youtube_ingestion_pipeline.py
@@ -0,0 +1,10 @@
+
+from src.Llama_index_sandbox.ingestion_pipeline import main as ingestion_pipeline_main
+
+
+def run_all():
+    ingestion_pipeline_main()
+
+
+if __name__ == "__main__":
+    run_all()
